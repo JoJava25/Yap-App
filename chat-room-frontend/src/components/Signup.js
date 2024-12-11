cf48c280d0ef83f3b7708002aacef98a0f210411
JoJava25 <trogdor2k5@hotmail.com>
2024-12-10 19:39:30 -0500

    set up signup and messaging components

---
+++ chat-room-frontend/src/components/Signup.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react'
+
+const SignupForm = ({onSignup}) => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [email, setEmail] = useState('')
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+
+        const response = await fetch('http://localhost:8000/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({username, email, password}),
+        })
+
+        const data = await response.json()
+
+        onSignup(data.token, data.username)
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <input type='text' placeholder='username' value={username} onChange={(e) => setUsername(e.target.value)}/>
+            <input type='text' placeholder='password' value={password} onChange={(e) => setPassword(e.target.value)}/>
+            <input type='email' placeholder='email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+            <button type='submit'>Sign Up</button>
+        </form>
+    )
+}
+export default SignupForm
